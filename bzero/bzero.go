54bf900ab1b618f23c0839fbb2debe9651c28f03
Frank Braun <frank@cryptogroup.net>
2015-03-25 00:50:22 +0100

    add bzero.Struct()

--- bzero/bzero.go
+++ bzero/bzero.go
@@ -16,9 +16,32 @@ package bzero
  * OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
  */
 
+import (
+	"fmt"
+	"reflect"
+)
+
 // Bytes sets all entries in the given byte array buf to zero.
 func Bytes(buf []byte) {
 	for i := 0; i < len(buf); i++ {
 		buf[i] = 0
+	}
+}
+
+// Struct sets all entries in the given struct pointer strct to zero.
+// The struct definition must only contain arrays or slices, otherwise the
+// function panics.
+func Struct(strct interface{}) {
+	s := reflect.ValueOf(strct).Elem()
+	for i := 0; i < s.NumField(); i++ {
+		f := s.Field(i)
+		switch k := f.Kind(); k {
+		case reflect.Array:
+			Bytes(f.Slice(0, f.Len()).Bytes())
+		case reflect.Slice:
+			Bytes(f.Bytes())
+		default:
+			panic(fmt.Sprintf("bzero: cannot zero %s", k))
+		}
 	}
 }
--- bzero/bzero_test.go
+++ bzero/bzero_test.go
@@ -26,11 +26,45 @@ import (
 func TestBytes(t *testing.T) {
 	zero := make([]byte, 1024)
 	buf := make([]byte, 1024)
+	// compare new buffer
+	if !bytes.Equal(buf, zero) {
+		t.Error("buffers differ")
+	}
+	// fill buffer with random data
 	if _, err := io.ReadFull(rand.Reader, buf); err != nil {
 		t.Fatal(err)
 	}
+	// zero
 	Bytes(buf)
+	// compare reset buffer
 	if !bytes.Equal(buf, zero) {
+		t.Error("buffers differ")
+	}
+}
+
+type T struct {
+	A [1024]byte
+	B []byte
+}
+
+func TestStruct(t *testing.T) {
+	zero := T{B: make([]byte, 1024)}
+	strct := T{B: make([]byte, 1024)}
+	// compare new structs
+	if !bytes.Equal(strct.A[:], zero.A[:]) || !bytes.Equal(strct.B, zero.B) {
+		t.Error("buffers differ")
+	}
+	// fill struct with random data
+	if _, err := io.ReadFull(rand.Reader, strct.A[:]); err != nil {
+		t.Fatal(err)
+	}
+	if _, err := io.ReadFull(rand.Reader, strct.B); err != nil {
+		t.Fatal(err)
+	}
+	// zero
+	Struct(&strct)
+	// compare resets structs
+	if !bytes.Equal(strct.A[:], zero.A[:]) || !bytes.Equal(strct.B, zero.B) {
 		t.Error("buffers differ")
 	}
 }
