64ebbc9ea0d7b3d2e145b7ca202b84642042b0a8
Frank Braun <frank@cryptogroup.net>
2015-11-04 02:25:04 +0000

    add LICENSE file

--- signify/signify.go
+++ signify/signify.go
@@ -1,29 +1,8 @@
+// Copyright (c) 2015 Frank Braun <frank@cryptogroup.net>
+// Use of this source code is governed by an ISC
+// license that can be found in the LICENSE file.
+
 package signify
-
-/*
- * Copyright (c) 2015 Frank Braun <frank@cryptogroup.net>
- *
- * Permission to use, copy, modify, and distribute this software for any
- * purpose with or without fee is hereby granted, provided that the above
- * copyright notice and this permission notice appear in all copies.
- *
- * THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES
- * WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF
- * MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR
- * ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES
- * WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN
- * ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF
- * OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
- */
-
-/*
-   This code is heavily based on C code from OpenBSD in src/usr.bin/signify/ which is
-   Copyright (c) 2013 Ted Unangst <tedu@openbsd.org>
-   covered by the same license as above.
-
-   "If I have seen further it is by standing on the shoulders of giants."
-   -- Isaac Newton
-*/
 
 import (
 	"bufio"
